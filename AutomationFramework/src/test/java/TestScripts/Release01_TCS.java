7e14a2e461a3d5929a61cfa9d2a10ece5e228f7d
PraveenManda <praveen.mtec@gmail.com>
2025-03-15 11:07:45 +0530

    Updates Some changes in Release TC01 File

--- AutomationFramework/src/test/java/TestScripts/Release01_TCS.java
+++ AutomationFramework/src/test/java/TestScripts/Release01_TCS.java
@@ -4,6 +4,9 @@ public class Release01_TCS {
 
 	public static void main(String[] args) {
 		
+		System.out.println("Hello I am praveen");
+		System.out.println("Hello I am praveen");
+		System.out.println("Hello I am praveen");
 
 	}
 
