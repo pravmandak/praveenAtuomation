13e8784a31d6fa05b494eaa5436ce35882f78695
PraveenManda <praveen.mtec@gmail.com>
2025-03-18 11:11:34 +0530

    Praveen changes

---
+++ AutomationFramework/src/test/java/Utitlities/readExcelDataToMap.java
@@ -0,0 +1,43 @@
+package Utitlities;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.Test;
+
+public class readExcelDataToMap {
+
+	@Test
+	public void geekforgeeks() throws IOException {
+
+		FileInputStream file = new FileInputStream("C:\\AutomationFramework\\AutomationFramework\\TestData\\DemoFile.xlsx");
+		XSSFWorkbook wb = new XSSFWorkbook(file);
+		XSSFSheet sh = wb.getSheet("Sheet1");
+
+		HashMap<Integer, String> map = new HashMap<Integer, String>();
+
+		for (int r = 0; r <= sh.getLastRowNum(); r++) {
+			int key = (int) sh.getRow(r).getCell(0).getNumericCellValue();
+			String value = sh.getRow(r).getCell(1).getStringCellValue();
+			map.put(key, value);
+		}
+
+		// Displaying HashMap
+		Iterator<Entry<Integer, String>> new_Iterator = map.entrySet().iterator();
+
+		while (new_Iterator.hasNext()) {
+			Map.Entry<Integer, String> new_Map = (Map.Entry<Integer, String>) new_Iterator.next();
+
+			System.out.println(new_Map.getKey() + "|" + new_Map.getValue());
+		}
+		wb.close();
+		file.close();
+	}
+
+}
